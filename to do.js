95459424c093f5c6a84b131c69e9d3d927d6cb81
Mahin <mahinarafatmeem@gmail.com>
2018-07-23 16:57:08 +0600

    a to do list created by using JavaScript

---
+++ to do.js
@@ -0,0 +1,57 @@
+
+// create close button
+
+var Nodelist = document.getElementsByTagName("LI");
+for ( var i = 0; i < Nodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  Nodelist[i].appendChild(span);
+}
+
+//close button on click
+
+var close = document.getElementsByClassName("close");
+for ( var i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// checked symbol
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item onclick-add button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("Input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("UL").appendChild(li);
+  }
+  document.getElementById("Input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
